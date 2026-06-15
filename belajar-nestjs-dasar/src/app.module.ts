cda9d5f0ed89ac13c5c63ce8e863267d45891e5a
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-06-15 10:24:34 +0700

    middleware

--- belajar-nestjs-dasar/src/app.module.ts
+++ belajar-nestjs-dasar/src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -8,6 +13,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { WinstonModule } from 'nest-winston';
 import { ValidationModule } from './validation/validation.module';
 import * as winston from 'winston';
+import { LogMiddleware } from './log/log.middleware';
 
 @Module({
   imports: [
@@ -27,4 +33,11 @@ import * as winston from 'winston';
   controllers: [AppController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes({
+      path: '/api/*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
