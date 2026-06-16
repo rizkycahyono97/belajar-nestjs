f1345e515ce1857cfe2b48c0b583d7d57047ea0d
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-06-16 09:55:47 +0700

    add winston

--- belajar-nestjs-restful-api/src/app.module.ts
+++ belajar-nestjs-restful-api/src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+    //configModule = untuk mengambil seperti .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
