146c0d025fc2aba1c137fdc72b50be56a31a26dc
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2026-06-16 10:59:18 +0700

    add user module

--- belajar-nestjs-restful-api/src/app.module.ts
+++ belajar-nestjs-restful-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { UserModule } from './user/user.module';
 
 @Global()
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UserModule],
   controllers: [],
   providers: [],
 })
